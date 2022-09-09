aafb9ab9185e7b4d56a5fd1d6802e934df87f0b4
Tobias Weiss <tobias@tobias-weiss.org>
2022-09-09 13:06:54 +0200

    add events

--- Assets/Scripts/EventSource.cs
+++ Assets/Scripts/EventSource.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventSource : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public event EventHandler OnSpacePressed;
+
+
+    public class OnSpacePressedEventArgs : EventArgs
     {
-        
+        public int spaceCount;
     }
 
-    // Update is called once per frame
+
+    public event EventHandler<OnSpacePressedEventArgs> OnSpacePressedWithArgs;
+    private int spaceCount;
+
+    public delegate void FloatEvent(float f);
+    public event FloatEvent OnFloatEvent;
+
+    public event Action<bool, int> OnActionEvent;
+
+    public UnityEvent OnUnityEvent;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnSpacePressed?.Invoke(this, EventArgs.Empty);
+
+            spaceCount++;
+            OnSpacePressedWithArgs?.Invoke(this, new OnSpacePressedEventArgs { spaceCount = spaceCount });
+            
+            OnFloatEvent?.Invoke(5.5f);
+
+            OnActionEvent?.Invoke(true, 123);
+
+            OnUnityEvent?.Invoke();
+        }
+
     }
 }
--- Assets/Scripts/EventSubscriber.cs
+++ Assets/Scripts/EventSubscriber.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Tracing;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +9,35 @@ public class EventSubscriber : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        EventSource eventSource = GetComponent<EventSource>();
+        eventSource.OnSpacePressed += eventSource_OnSpacePressed;
+        eventSource.OnSpacePressedWithArgs += eventSource_OnSpacePressedWithArgs;
+        eventSource.OnFloatEvent += eventSource_OnFloatEvent;
+        eventSource.OnActionEvent += eventSource_OnActionEvent;
     }
 
     // Update is called once per frame
-    void Update()
+    private void eventSource_OnSpacePressed(object sender, EventArgs e)
     {
-        
+        Debug.Log("Space pressed");
+    }
+    private void eventSource_OnSpacePressedWithArgs(object sender, EventSource.OnSpacePressedEventArgs e)
+    {
+        Debug.Log($"Space pressed {e.spaceCount} times");
+    }
+
+    private void eventSource_OnFloatEvent(float f)
+    {
+        Debug.Log("Float event: " + f.ToString());
+    }
+
+    private void eventSource_OnActionEvent(bool arg1, int arg2)
+    {
+        Debug.Log($"Arg1: {arg1}, Arg2: {arg2}");
+    }
+
+    public void eventSource_OnUnityEvent()
+    {
+        Debug.Log("Unity event");
     }
 }
